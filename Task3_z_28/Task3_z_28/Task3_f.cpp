0beff746b03d1ae7ea6eb3f215c13a0924be955f
Roman <Bojko.r91@gmail.com>
2014-03-02 20:15:07 +0200

    Task3_z_28
    String

---
+++ Task3_z_28/Task3_z_28/Task3_f.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	cout <<"Enter Your String "<<endl;
+	char string[255];
+	cin.getline(string,255);
+	cout << "-----------"<<endl;
+	cout <<"Your String :  ";
+	cout << string <<endl;
+    cout << "-----------"<<endl;
+	
+	cout <<"Enter Next String "<<endl;
+	char string1[255];
+	cin.getline(string1,255);
+	cout << "-----------"<<endl;
+	cout <<"Your Next String :  ";
+	cout << string1 <<endl;
+	cout << "-----------"<<endl;
+	
+	char symbol;
+	int j = 0;
+	cout<<"Enter Search Symbol: ";
+    cin>>symbol;
+	
+
+	for(int i = 0; string[i]!='\0'; ++i)
+    {
+		
+        if(string[i] == symbol)
+            {
+				j++;
+			}
+		
+		}
+	cout<<"Symbol "<<"' "<<symbol<<" '"<<" found in first string "<<j<<" raz."<<endl;
+	j = 0;
+
+		for(int i = 0; string1[i]!='\0'; ++i)
+    {
+		
+        if(string1[i] == symbol)
+            {
+				
+				j++;
+			}
+	    }
+	cout << "-----------"<<endl;
+    cout<<"Symbol "<<"' "<<symbol<<" '"<<" found in second string "<<j<<" raz."<<endl;
+    system("pause");
+    return 0;   
+	
+}
